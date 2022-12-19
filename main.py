544c60de8e7df84f9904866bf601c981ed9e7348
Cagatay <cagatay.yildiz1@gmail.com>
2022-12-19 22:18:59 +0100

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 # 2180016 - not invariant - 2182170
 
 # 2182188 - invariant
-# 2182197 - invariant CNN
+# 2182198 - invariant CNN
 # 2182191 - not invariant
 
 from model.model_misc import build_model, train_model
@@ -33,7 +33,7 @@ parser.add_argument('--num_workers', type=int, default=0,
                     help="number of workers")
 parser.add_argument('--data_root', type=str, default='data/',
                     help="general data location")
-parser.add_argument('--Ntrain', type=int, default=500,
+parser.add_argument('--Ntrain', type=int, default=360,
                     help="Number training data points")
 parser.add_argument('--Nvalid', type=int, default=40,
                     help="Number valid data points")
@@ -43,7 +43,7 @@ parser.add_argument('--digit', type=int, default=3,
                     help="Rotating MNIST digit (train data)")
 
 #de model
-parser.add_argument('--ode_latent_dim', type=int, default=2,
+parser.add_argument('--ode_latent_dim', type=int, default=4,
                     help="Latent ODE dimensionality")
 parser.add_argument('--de', type=str, default='SVGP', choices=DE_MODELS,
                     help="Model type to learn the DE")
@@ -67,7 +67,7 @@ parser.add_argument('--num_hidden', type=int, default=200,
                     help="Number of hidden neurons in each layer of MLP diff func")
 
 #inavariance gp
-parser.add_argument('--inv_latent_dim', type=int, default=6,
+parser.add_argument('--inv_latent_dim', type=int, default=4,
                     help="Invariant space dimensionality")
 parser.add_argument('--num_inducing_inv', type=int, default=100,
                     help="Number of inducing points for inavariant GP")
