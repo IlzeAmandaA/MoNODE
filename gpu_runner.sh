c1f3cde834e34dd04c68e3ca764c381022b10a1c
Ilze Auzina <iauzina@ivi-h0.ivi.local>
2023-01-10 10:37:15 +0100

    run file efit

--- gpu_runner.sh
+++ gpu_runner.sh
@@ -6,13 +6,13 @@
 #SBATCH --mem=30G                 # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --cpus-per-task=10        # cpus per task
 #SBATCH --output=/home/iauzina/slurm_logs/%x_%j.out  # File to which STDOUT will be written
-#SBATCH --error=/home/iazuina/slurm_logs/%x_%j.err   # File to which STDERR will be written
-
+#SBATCH --error=/home/iauzina/slurm_logs/%x_%j.err   # File to which STDERR will be written
+#SBATCH -D /home/iauzina/InvOdeVae
 
 # include information about the job in the output
 scontrol show job=$SLURM_JOB_ID
 
-# conda init bash
+# conda activate virtual venv
 source activate venv3.8
-# conda  activate default
-srun python main.py --Nepoch 100
+# Run Script
+srun -D `pwd` python main.py --Nepoch 100
